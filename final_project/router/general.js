01187ca95118845d23ef76552de078e0122a9cbb
H <>
2025-02-14 18:44:43 -0500

    Added GET for author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,25 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let foundBooks = {};
+
+  Object.keys(books).forEach(key => {
+    const book = books[key];
+    if(book.author === author)
+    {
+        foundBooks[book.title] = book;
+    }
+  });
+
+  if (Object.keys(foundBooks) !== 0)
+  {
+    res.send(foundBooks)
+  }
+  else
+  {
+    res.send("Book not found for author " + author);
+  }
 });
 
 // Get all books based on title
