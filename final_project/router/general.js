242f3445147645b25c70bd2390c93f860741548a
H <>
2025-02-14 19:12:47 -0500

    Added login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,6 +18,11 @@ const doesExist = (username) => {
     }
 }
 
+// For debugging!
+public_users.get("/users", (req, res) => {
+    res.send(users);
+});
+
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
